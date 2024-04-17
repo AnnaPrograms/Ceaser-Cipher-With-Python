0a0af41e9ca03705060ba47b9b748eb1438b9194
Anna S <138823226+AnnaPrograms@users.noreply.github.com>
2024-04-16 22:51:05 -0500

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,32 @@
+"""
+
+Lab 4
+Name: Anna Stow 
+Class: CSCI 161: Computer Science II - 11335 - Spr24 Online
+
+"""
+
+user_string = input("Please enter a message to find the frequency of words: ").lower()
+
+
+# removes punctuation
+user_string = "".join(filter(lambda x: x.isalpha() or x.isspace(), user_string)).split()
+
+# empty dictionary data base
+db = {}
+
+# removes duplicates as a constraint
+new_string = set(user_string)
+    
+for i in new_string:
+    
+    # adds frequencies to the database dictionary
+    db[i] = user_string.count(i)
+
+# grabs second value in dict to sort by min, then reverse to switch to max
+for i, j in sorted(db.items(), key = lambda x: x[1], reverse = True):
+
+    # display's sorted dict
+    print(f"{i} : {j}")
+
+
